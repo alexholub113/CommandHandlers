480cdfe424cab75c6247bf43f89e101ccae6cc6a
Alex Holub <alexholub@microsoft.com>
2025-04-13 16:01:49 -0700

    Moved files

---
+++ src/AspNetCore.CommandHandlers/Abstractions/CommandConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace AspNetCore.CommandHandlers.Abstractions;
+
+public class CommandConstants
+{
+    public const string RoutePrefix = "api/v1";
+
+    public const string CommandMethodName = "Handle";
+
+    public const string CommandTypeSuffix = "Handler";
+
+    public const string CommandNamespacePart = "Commands";
+}
---
+++ src/AspNetCore.CommandHandlers/Abstractions/CommandHandlerAttribute.cs
@@ -0,0 +1,6 @@
+namespace AspNetCore.CommandHandlers.Abstractions;
+
+public class CommandHandlerAttribute : Attribute
+{
+
+}
